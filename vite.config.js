d5381d6a1c9cb2cc77af75f576839a557f9ec8fb
lam <volethanhlam@192.168.1.111.non-exists.ptr.local>
2025-04-03 19:28:05 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+   
+   
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
